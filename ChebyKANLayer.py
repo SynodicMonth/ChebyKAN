e6b79303e6ead5b85bd392c7b2b0fc7051ae39d6
K-H-Ismail <khalfaoui.ismail@hotmail.com>
2024-05-12 20:41:32 +0200

    Vectorization of <for loop> on Chebychev degree

--- ChebyKANLayer.py
+++ ChebyKANLayer.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import numpy as np
+
 
 # This is inspired by Kolmogorov-Arnold Networks but using Chebyshev polynomials instead of splines coefficients
 class ChebyKANLayer(nn.Module):
@@ -11,20 +11,26 @@ class ChebyKANLayer(nn.Module):
         self.degree = degree
 
         self.cheby_coeffs = nn.Parameter(torch.empty(input_dim, output_dim, degree + 1))
-        nn.init.normal_(self.cheby_coeffs, mean=0.0, std=1/(input_dim * (degree + 1)))
+        nn.init.normal_(self.cheby_coeffs, mean=0.0, std=1 / (input_dim * (degree + 1)))
+        self.register_buffer("arange", torch.arange(0, degree + 1, 1))
 
     def forward(self, x):
-        x = torch.reshape(x, (-1, self.inputdim))  # shape = (batch_size, inputdim)
         # Since Chebyshev polynomial is defined in [-1, 1]
         # We need to normalize x to [-1, 1] using tanh
         x = torch.tanh(x)
-        # Initialize Chebyshev polynomial tensors
-        cheby = torch.ones(x.shape[0], self.inputdim, self.degree + 1, device=x.device)
-        if self.degree > 0:
-            cheby[:, :, 1] = x
-        for i in range(2, self.degree + 1):
-            cheby[:, :, i] = torch.cos(i * torch.acos(x))
+        # View and repeat input degree + 1 times
+        x = x.view((-1, self.inputdim, 1)).expand(
+            -1, -1, self.degree + 1
+        )  # shape = (batch_size, inputdim, self.degree + 1)
+        # Apply acos
+        x = x.acos()
+        # Multiply by arange [0 .. degree]
+        x *= self.arange
+        # Apply cos
+        x = x.cos()
         # Compute the Chebyshev interpolation
-        y = torch.einsum('bid,iod->bo', cheby, self.cheby_coeffs)  # shape = (batch_size, outdim)
+        y = torch.einsum(
+            "bid,iod->bo", x, self.cheby_coeffs
+        )  # shape = (batch_size, outdim)
         y = y.view(-1, self.outdim)
         return y
